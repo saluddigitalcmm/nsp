e9e7aa818a5b49592e7ea17059f71846236c2ec6
Fabián Villena <villenafabian@gmail.com>
2020-03-23 11:41:16 -0300

    split. train and evaluate

--- analyze.py
+++ analyze.py
@@ -1,5 +1,7 @@
 from src.data.data_consolidation import Consolidator
 from src.features.basic_features import Featurizer
+from src.models.nsp_classifier import Trainer, Evaluator
+from src.data.dataset_generator import Splitter
 
 c = Consolidator(r"data/raw/informe_estadistico/",r"data/interim/Base.csv")
 c.consolidate()
@@ -7,3 +9,12 @@ c.consolidate()
 f = Featurizer("data/interim/Base.csv")
 f.generate_features()
 f.write("data/interim/data.csv")
+
+s = Splitter("data/interim/data.csv")
+s.split("data/processed/")
+
+t = Trainer("data/processed/features_train.csv","data/processed/label_train.csv")
+t.fit("models/nsp_classifier.joblib")
+
+e = Evaluator("models/nsp_classifier.joblib","data/processed/features_test.csv","data/processed/label_test.csv")
+e.evaluate("reports/classification_report.json")
