534c1c3228b1e729e06a971ffe5f3f86219c469d
Fabián Villena <villenafabian@gmail.com>
2020-03-27 16:34:01 -0300

    age as categorical and p_nsp

--- src/features/basic_features.py
+++ src/features/basic_features.py
@@ -2,16 +2,16 @@ import pandas as pd
 import sklearn.preprocessing
 import logging
 import numpy as np
+import sqlite3
+from io import StringIO
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 class Featurizer:
     def __init__(self,interim_dataset_location):
         self.data = pd.read_csv(interim_dataset_location)
-        self.data.drop(columns=["PAID"], inplace=True, axis=1)
-
         logger.info("current shape: {}".format(self.data.shape))
-    def generate_features(self):
+    def generate_basic_features(self):
         self.data["FechaCita"] = pd.to_datetime(self.data["FechaCita"])
         self.data["HoraCita"] = pd.to_datetime(self.data["HoraCita"])
         self.data["FechaNac"] = pd.to_datetime(self.data["FechaNac"])
@@ -23,7 +23,8 @@ class Featurizer:
         self.data = self.data[self.data["EstadoCita"].isin(["Atendido","No Atendido"])]
         self.data["NSP"] = np.where(self.data["EstadoCita"] == "No Atendido",1,0)
 
-        self.data.drop(columns=["FechaCita","HoraCita","FechaNac","EstadoCita"], inplace=True, axis=1)
+        self.data_to_history = self.data[["PAID","Especialidad","FechaCita"]]
+        self.data = self.data.drop(columns=["PAID","FechaCita","HoraCita","FechaNac","EstadoCita"], axis=1)
         logger.info("current shape: {}".format(self.data.shape))
 
         self.data = self.data[(self.data["age"] <= 15) & (self.data["age"] >= 0)]
@@ -31,11 +32,56 @@ class Featurizer:
         self.data["hour"] = self.data["hour"].astype('category')
         logger.info("current shape: {}".format(self.data.shape))
 
-        age_scaler = sklearn.preprocessing.MinMaxScaler()
-        self.data["age"] = age_scaler.fit_transform(self.data[["age"]])
+        self.data["age"] = pd.cut(self.data["age"],[0,0.5,5,12,18],right=False,labels=["lactante","infante_1","infante_2","adolescente"])
 
         self.data = pd.get_dummies(self.data)
         logger.info("current shape: {}".format(self.data.shape))
+    def generate_history_feature(self,db_location):
+        conn = sqlite3.connect(db_location)
+        cur = conn.cursor()
+        def get_history_from_db(PAID,Especialidad,FechaCita,span):
+            cur.execute("""
+            SELECT sum(NSP) as NSP_count, count(NSP) as citation_count
+            FROM history
+            WHERE PAID = {}
+                AND Especialidad = "{}"
+                AND FechaCita >= date("{}", "-{} month")
+                AND FechaCita < date("{}")
+            """.format(PAID,Especialidad,FechaCita,span,FechaCita)
+            )
+            row = cur.fetchone()
+            if row[1] == 0:
+                p_NSP = 0.5
+            else:
+                try:
+                    p_NSP = row[0] / row[1]
+                except TypeError:
+                    p_NSP = 0.5
+            logger.info("{} {} {} {} {} {}".format(PAID,Especialidad,FechaCita,row[0],row[1],p_NSP))
+            return p_NSP
+
+        def get_history_from_db_simple(df):
+            history = pd.read_sql("""
+            
+            SELECT
+                PAID,
+                Especialidad,
+                sum(NSP) as NSP_count,
+                count(NSP) as citation_count
+            FROM
+                history
+            GROUP BY
+                PAID,
+                Especialidad
+            
+            """,conn)
+            logger.info(df.shape)
+            df = df.merge(history,on=["PAID","Especialidad"],how="left")
+            logger.info(df.shape)
+            return df["NSP_count"] / df["citation_count"]
+        logger.info(self.data.shape)
+        self.data["p_NSP"] = get_history_from_db_simple(self.data_to_history[["PAID","Especialidad"]]).fillna(value=0.5)
+
     def write(self,data_location):
         self.data.to_csv(data_location,index=False)
 
