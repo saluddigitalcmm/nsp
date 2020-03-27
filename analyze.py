b4998e3fa54db4743432b3d61e40a6d305edbd48
Fabián Villena <villenafabian@gmail.com>
2020-03-27 16:34:09 -0300

    history

--- analyze.py
+++ analyze.py
@@ -2,12 +2,18 @@ from src.data.data_consolidation import Consolidator
 from src.features.basic_features import Featurizer
 from src.models.nsp_classifier import Trainer, Evaluator
 from src.data.dataset_generator import Splitter
+from src.features.history import HistoryCreator
 
 c = Consolidator(r"data/raw/informe_estadistico/",r"data/interim/Base.csv")
 c.consolidate()
 
+h = HistoryCreator("data/interim/Base.csv")
+h.create_history()
+h.write_db("data/processed/history.sqlite")
+
 f = Featurizer("data/interim/Base.csv")
-f.generate_features()
+f.generate_basic_features()
+f.generate_history_feature("data/processed/history.sqlite")
 f.write("data/interim/data.csv")
 
 s = Splitter("data/interim/data.csv")
