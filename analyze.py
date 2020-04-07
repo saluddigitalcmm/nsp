bd7155c5a94e81ebe14003385bccded090b570f4
Fabián Villena <villenafabian@gmail.com>
2020-04-07 15:04:48 -0400

    train and predict best models

--- analyze.py
+++ analyze.py
@@ -1,6 +1,5 @@
 from src.data.data_consolidation import Consolidator
 from src.features.basic_features import Featurizer
-from src.models.nsp_classifier import Trainer, Evaluator
 from src.data.dataset_generator import Splitter
 from src.features.history import HistoryCreator
 from src.models.nsp_classifier import NspModelDev
@@ -22,12 +21,11 @@ f.write("data/interim/data.csv")
 s = Splitter("data/interim/data.csv")
 s.split("data/processed/")
 
-t = Trainer("data/processed/features_train.csv","data/processed/label_train.csv")
-t.fit("models/nsp_classifier.joblib")
-
-d = NspModelDev("data/processed/features_train.csv","data/processed/label_train.csv","data/processed/features_test.csv","data/processed/label_test.csv",30000)
+d = NspModelDev("data/processed/features_train.csv","data/processed/label_train.csv",30000)
 d.grid_search("reports/grid_search/",n_jobs=-1)
 d.train_models("reports/grid_search/",-1,"reports/cross_val/")
+d.train_best_models("models/","reports/grid_search/")
+d.predict_best_models("models/","data/processed/features_test.csv","data/processed/label_test.csv")
 
 cross_val_viz = CrossValVisualizer('reports/cross_val/')
 cross_val_viz.plot('reports/figures/cross_val.pdf')
