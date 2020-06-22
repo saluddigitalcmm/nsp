21943968be23187b7542e8eb336e1b7136223cd1
Fabián Villena <villenafabian@gmail.com>
2020-06-22 16:42:14 -0400

    consolidator and featurizer for hcm

--- src/data/data_consolidation.py
+++ src/data/data_consolidation.py
@@ -11,7 +11,7 @@ logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 class Consolidator:
-    def __init__(self,raw_data_folder,consolidated_data_location,citas_excel_location):
+    def __init__(self,raw_data_folder,consolidated_data_location,citas_excel_location=None):
         ## Abrir y leer archivos Excel
         self.consolidated_data_location = consolidated_data_location
         self.directorio = raw_data_folder
@@ -22,24 +22,30 @@ class Consolidator:
         # Expresion regular que busca archivos con nombre INFORME ESTADISTICO
         self.newlist_Informe = []
         for filename in os.listdir(self.directorio):
-            if filename.endswith("xlsx"):
+            if (filename.endswith("xlsx") & filename.startswith("~$") == False):
                 self.newlist_Informe.append(self.directorio+filename)
-        self.citas_excel_location = citas_excel_location
+        if citas_excel_location != None:
+            self.citas_excel_location = citas_excel_location
+        else:
+            self.citas_excel_location = None
         logger.info( 'Los informes excel son {}'.format(self.newlist_Informe))
 
-    def consolidate(self):
+    def consolidate(self,idx="PAID"):
         # El archivo Base.csv sera donde se guarde la base de datos completa. 
         # Si no esta en el directorio, se crea
         
         
-        columns = ["PAID",u'FechaNac', u'Sexo', u'Comuna', u'Prevision',
+        columns = [idx,u'FechaNac', u'Sexo', u'Comuna', u'Prevision',
                   u'Especialidad', u'TipoAtencion', 'TipoProfesional', 'CodPrestacion',
                   u'FechaCita', u'EstadoCita', u'HoraCita']
-        self.citas_datos = pd.read_excel(self.citas_excel_location, parse_dates=['FechaNac','FechaCita','FechaReserva'])[columns[:-1] + ['FechaReserva']]
-        self.citas_datos["PAID"] = pd.to_numeric(self.citas_datos["PAID"],errors="coerce",downcast="integer")
-        logger.info("citas_datos shape: {}".format(self.citas_datos.shape))
-        self.citas_datos.dropna(inplace=True)
-        logger.info("citas_datos shape: {}".format(self.citas_datos.shape))
+        if self.citas_excel_location != None:
+            self.citas_datos = pd.read_excel(self.citas_excel_location, parse_dates=['FechaNac','FechaCita','FechaReserva'])[columns[:-1] + ['FechaReserva']]
+            self.citas_datos[idx] = pd.to_numeric(self.citas_datos[idx],errors="coerce",downcast="integer")
+            logger.info("citas_datos shape: {}".format(self.citas_datos.shape))
+            self.citas_datos.dropna(inplace=True)
+            logger.info("citas_datos shape: {}".format(self.citas_datos.shape))
+        else:
+            self.citas_datos = None
         logger.info('Leer todos los excels para compilarlos')
         for archivo in self.newlist_Informe:
 
@@ -48,7 +54,7 @@ class Consolidator:
             # Leo la primera hoja del excel
             try:
                 DatosDF = df.parse(sheet_name=df.sheet_names[0],parse_dates=['FechaNac','FechaCita'])
-                DatosDF["PAID"] = pd.to_numeric(DatosDF["PAID"],errors="coerce",downcast="integer")
+                # DatosDF[idx] = pd.to_numeric(DatosDF[idx],errors="coerce",downcast="integer")
             
                 DatosDF = DatosDF[columns]
                 self.Datos = pd.concat([self.Datos,DatosDF])
@@ -60,7 +66,8 @@ class Consolidator:
         logger.info("Datos shape: {}".format(self.Datos.shape))
         self.Datos.dropna(inplace=True)
         logger.info("Datos shape: {}".format(self.Datos.shape))
-        self.Datos = self.Datos.merge(self.citas_datos[['FechaNac','FechaCita','CodPrestacion','FechaReserva']],how="left")
+        if self.citas_datos != None:
+            self.Datos = self.Datos.merge(self.citas_datos[['FechaNac','FechaCita','CodPrestacion','FechaReserva']],how="left")
         logger.info("Datos shape: {}".format(self.Datos.shape))
         logger.info(self.Datos.columns)
 
--- src/features/basic_features.py
+++ src/features/basic_features.py
@@ -11,12 +11,13 @@ class Featurizer:
     def __init__(self,interim_dataset_location):
         self.data = pd.read_csv(interim_dataset_location)
         logger.info("current shape: {}".format(self.data.shape))
-    def generate_basic_features(self):
+    def generate_basic_features(self,use_reserve=True, idx="PAID"):
         self.data["FechaCita"] = pd.to_datetime(self.data["FechaCita"])
         self.data["HoraCita"] = pd.to_datetime(self.data["HoraCita"])
         self.data["FechaNac"] = pd.to_datetime(self.data["FechaNac"])
-        self.data["FechaReserva"] = pd.to_datetime(self.data["FechaReserva"])
-        self.data["delay"] = (self.data["FechaCita"]-self.data["FechaReserva"]).astype('timedelta64[W]')
+        if use_reserve:
+            self.data["FechaReserva"] = pd.to_datetime(self.data["FechaReserva"])
+            self.data["delay"] = (self.data["FechaCita"]-self.data["FechaReserva"]).astype('timedelta64[W]')
         self.data["age"] = (self.data["FechaCita"]-self.data["FechaNac"]).astype('timedelta64[D]')/365.25
         self.data["month"] = self.data["FechaCita"].dt.month_name()
         self.data["day"] = self.data["FechaCita"].dt.day_name()
@@ -29,14 +30,16 @@ class Featurizer:
         logger.info("current shape: {}".format(self.data.shape))
 
         self.data = self.data[(self.data["age"] <= 15) & (self.data["age"] >= 0)]
-        self.data = self.data[(self.data["delay"] <= 10) & (self.data["delay"] >= 0)]
+        if use_reserve:
+            self.data = self.data[(self.data["delay"] <= 10) & (self.data["delay"] >= 0)]
         self.data = self.data[(self.data["hour"] <= 17) & (self.data["hour"] >= 8)]
         self.data["hour"] = self.data["hour"].astype('category')
-        self.data["delay"] = self.data["delay"].astype('category')
+        if use_reserve:
+            self.data["delay"] = self.data["delay"].astype('category')
         logger.info("current shape: {}".format(self.data.shape))
 
         self.data["age"] = pd.cut(self.data["age"],[0,0.5,5,12,18],right=False,labels=["lactante","infante_1","infante_2","adolescente"])
-        self.data_to_history = self.data[["PAID","Especialidad","FechaCita"]]
+        self.data_to_history = self.data[[idx,"Especialidad","FechaCita"]]
 
         ## Crear clasificacion TipoPrestacion medica/no medica a partir de los codigos FONASA
 
@@ -91,24 +94,27 @@ class Featurizer:
         #print ''
         #
         #print DD.loc[DD['TipoProfesionalC']=='NoMedico']['TipoProfesional'].value_counts()
-
-        self.data = self.data.drop(columns=["PAID","FechaCita","HoraCita","FechaNac","FechaReserva","EstadoCita",'TipoProfesional', 'CodPrestacion'], axis=1)
+        if use_reserve:
+            self.data = self.data.drop(columns=[idx,"FechaCita","HoraCita","FechaNac","FechaReserva","EstadoCita",'TipoProfesional', 'CodPrestacion'], axis=1)
+        else:
+            self.data = self.data.drop(columns=[idx,"FechaCita","HoraCita","FechaNac","EstadoCita",'TipoProfesional', 'CodPrestacion'], axis=1)
         logger.info(self.data.columns)
         self.data = pd.get_dummies(self.data)
         logger.info("current shape: {}".format(self.data.shape))
+        self.idx = idx
 
     def generate_history_feature(self,db_location):
         conn = sqlite3.connect(db_location)
         cur = conn.cursor()
-        def get_history_from_db(PAID,Especialidad,FechaCita,span):
+        def get_history_from_db(idx,idx_name,Especialidad,FechaCita,span):
             cur.execute("""
             SELECT sum(NSP) as NSP_count, count(NSP) as citation_count
             FROM history
-            WHERE PAID = {}
+            WHERE {} = {}
                 AND Especialidad = "{}"
                 AND FechaCita >= date("{}", "-{} month")
                 AND FechaCita < date("{}")
-            """.format(PAID,Especialidad,FechaCita,span,FechaCita)
+            """.format(idx,idx_name,Especialidad,FechaCita,span,FechaCita)
             )
             row = cur.fetchone()
             if row[1] == 0:
@@ -118,30 +124,48 @@ class Featurizer:
                     p_NSP = row[0] / row[1]
                 except TypeError:
                     p_NSP = 0.5
-            logger.info("{} {} {} {} {} {}".format(PAID,Especialidad,FechaCita,row[0],row[1],p_NSP))
+            logger.info("{} {} {} {} {} {}".format(idx,Especialidad,FechaCita,row[0],row[1],p_NSP))
             return p_NSP
 
         def get_history_from_db_simple(df):
             history = pd.read_sql("""
             
             SELECT
-                PAID,
+                {},
                 Especialidad,
                 sum(NSP) as NSP_count,
                 count(NSP) as citation_count
             FROM
                 history
             GROUP BY
-                PAID,
+                {},
                 Especialidad
             
-            """,conn)
+            """.format(self.idx,self.idx),conn)
             logger.info(df.shape)
-            df = df.merge(history,on=["PAID","Especialidad"],how="left")
+            df = df.merge(history,on=[self.idx,"Especialidad"],how="left")
+            logger.info(df.shape)
+            return df["NSP_count"] / df["citation_count"]
+        def get_history_from_db_simple_general(df):
+            history = pd.read_sql("""
+            
+            SELECT
+                {},
+                sum(NSP) as NSP_count,
+                count(NSP) as citation_count
+            FROM
+                history
+            GROUP BY
+                {}
+            
+            """.format(self.idx,self.idx),conn)
+            logger.info(df.shape)
+            df = df.merge(history,on=[self.idx],how="left")
             logger.info(df.shape)
             return df["NSP_count"] / df["citation_count"]
         logger.info(self.data.shape)
-        self.data["p_NSP"] = get_history_from_db_simple(self.data_to_history[["PAID","Especialidad"]]).fillna(value=0.5)
+        self.data["p_NSP"] = get_history_from_db_simple(self.data_to_history[[self.idx,"Especialidad"]])
+        self.data["p_NSP_g"] = get_history_from_db_simple_general(self.data_to_history[[self.idx]])
 
     def write(self,data_location):
         self.data.dropna(inplace=True)
